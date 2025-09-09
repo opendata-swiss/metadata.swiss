eccffac43939e022f6b4ff2070cc824638081fed
magdasalatka <magdalena@datali.ch>
2025-09-09 12:16:05 +0200

    migrate pipes

--- opendata.swiss/metadata/scripts/catalogues_delete.sh
+++ opendata.swiss/metadata/scripts/catalogues_delete.sh
@@ -7,3 +7,4 @@ HUB_REPO_ENDPOINT="https://piveau-hub-repo-ln.zazukoians.org"
 
 curl -i -X DELETE -H "X-API-Key: $API_KEY_HUB" -H "${HUB_REPO_ENDPOINT}/catalogues/staatskanzlei-kanton-zuerich"
 curl -i -X DELETE -H "X-API-Key: $API_KEY_HUB" -H "${HUB_REPO_ENDPOINT}/catalogues/bafu"
+curl -i -X DELETE -H "X-API-Key: $API_KEY_HUB" -H "${HUB_REPO_ENDPOINT}/catalogues/so-kt-geocat"
