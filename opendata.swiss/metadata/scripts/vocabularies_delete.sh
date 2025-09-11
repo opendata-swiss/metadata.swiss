d434bb1d2f96956e8d0e45f47145a4e72fafb792
Giacomo Citi <giacomociti@gmail.com>
2025-09-11 15:36:49 +0200

    Apply suggestions from code review
    Co-authored-by: Ludovic Muller <ludovicm67@users.noreply.github.com>

--- opendata.swiss/metadata/scripts/vocabularies_delete.sh
+++ opendata.swiss/metadata/scripts/vocabularies_delete.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 # Automatically export all variables
 set -a
 source .env
 set +a
 
-HUB_REPO_ENDPOINT="https://piveau-hub-repo-ln.zazukoians.org"
+
 
 curl -i -X DELETE -H "X-API-Key: $API_KEY_HUB" "${HUB_REPO_ENDPOINT}/vocabularies/ch-licenses"
