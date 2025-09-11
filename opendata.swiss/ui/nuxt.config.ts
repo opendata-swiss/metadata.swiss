fea4b01fa2fa1d6e96b96987f1e6f59be9c5abba
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-09-11 16:01:39 +0200

    refactor: prose components

--- opendata.swiss/ui/nuxt.config.ts
+++ opendata.swiss/ui/nuxt.config.ts
@@ -37,19 +37,22 @@ export default defineNuxtConfig({
       }
     }
   },
+  mdc: {
+    components: {
+      map: {
+        a: 'OdsProseA',
+        h2: 'OdsProseH2',
+        h3: 'OdsProseH3',
+        h4: 'OdsProseH4',
+      }
+    }
+  },
   components: {
     global: true,
     dirs: [
       '~/components',
       '~/components/content',
     ]
-  },
-  mdc: {
-    components: {
-      map: {
-        a: 'OdsProseA', // Map <a> tags to the OdsProseA component
-      }
-    }
   },
   build: {
     transpile: ['vuetify', 'form-data'],
