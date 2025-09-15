ef7ed1c56266aa73645d2ca36e2b10c2ab299db2
magdasalatka <magdalena@datali.ch>
2025-09-15 16:21:57 +0200

    transformatiom script

--- opendata.swiss/metadata/piveau_scripts/debug.js
+++ opendata.swiss/metadata/piveau_scripts/debug.js
@@ -1,26 +1,66 @@
-const fs = require('fs');
+function transforming(jsonBody) {
 
-function transforming(input, callback) {
-
+  let input;
   try {
-    let jsonBody;
     if (typeof input === 'string') {
-      jsonBody = JSON.parse(input);
+      input = JSON.parse(jsonBody);
     } else {
-      jsonBody = input;
+      input = jsonBody;
     }
 
-
-    console.log("Successfully processed object:");
-    console.log(JSON.stringify(jsonBody, null, 2)); // Pretty-print the object
-    fs.appendFileSync('/app/output.log', outputString + '\n');
-
-    // Use the callback to pass the result to the next step.
-    // This is the correct asynchronous pattern.
-    callback(null, jsonBody);
+      console.log("Successfully loaded object:");
+      outputString = JSON.stringify(input, null, 2)
+      console.log(outputString); // Pretty-print the object
   } catch (e) {
     // If the input is not valid JSON, fail gracefully.
     console.error("Failed to parse or process input:", e);
-    callback(e, null);
   }
+
+
+
+    let output = dcatapContext;
+
+    // Required
+    output["@type"] = "Dataset";
+
+    let language;
+    if (input["dc:language"]) {
+      if (input["dc:language"] === "ger") {
+        language = "de"
+      } else {
+        language = input["dc:language"];
+      }
+    } else
+      language = params.defaultLanguage;
+
+
+    output.title = piveau.mapLiteral(input["dc:title"], output.title, language)
+    output.description = piveau.mapLiteral(input["description"], output.description, language)
+
+    // Optional
+    output.page = piveau.mapReference(input["dc:URI"], output.page, "Document")
+
+
+    //dc:URI
+    //dc:date
+    //dc:description
+    //dc:format
+    //dc:identifier
+    //dc:subject
+    //dc:type
+    //dct:abstract
+    //dct:modified
+    //ows:BoundingBox
+    //ows:BoundingBox/ows:LowerCorner
+    //ows:BoundingBox/ows:UpperCorner
+
+    console.log("Transformed output:");
+    console.log(JSON.stringify(output, null, 2));
+
+    // Use the callback to pass the result to the next step.
+    // This is the correct asynchronous pattern.
+    // callback(null, jsonBody);
+
+    return output
 }
+
--- opendata.swiss/metadata/scripts/catalogues.sh
+++ opendata.swiss/metadata/scripts/catalogues.sh
@@ -6,4 +6,4 @@ set +a
 
 curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @opendata.swiss/metadata/piveau_catalogues/data-staatskanzlei-kanton-zuerich.ttl "${HUB_REPO_ENDPOINT}/catalogues/staatskanzlei-kanton-zuerich"
 curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @opendata.swiss/metadata/piveau_catalogues/data-bafu.ttl "${HUB_REPO_ENDPOINT}/catalogues/bafu"
-curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @opendata.swiss/metadata/piveau_catalogues/data-so-kt-geocat.ttl "${HUB_REPO_ENDPOINT}/catalogues/so-geocat"
+curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @opendata.swiss/metadata/piveau_catalogues/data-so-kt-geocat.ttl "${HUB_REPO_ENDPOINT}/catalogues/so-kt-geocat"
