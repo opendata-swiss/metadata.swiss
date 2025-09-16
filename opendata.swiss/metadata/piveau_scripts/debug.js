5cfa71884ae9ab5f3491922afd1dfdb4e81a9e2e
magdasalatka <magdalena@datali.ch>
2025-09-16 17:55:05 +0200

    debug.js

--- opendata.swiss/metadata/piveau_scripts/debug.js
+++ opendata.swiss/metadata/piveau_scripts/debug.js
@@ -1,16 +1,17 @@
 function transforming(jsonBody) {
 
+
   let input;
   try {
-    if (typeof input === 'string') {
+    if (typeof jsonBody === 'string') {
       input = JSON.parse(jsonBody);
     } else {
       input = jsonBody;
     }
 
-    console.log("Successfully loaded object:");
-    outputString = JSON.stringify(input, null, 2)
-    console.log(outputString); // Pretty-print the object
+    console.log("Successfully loaded input for transformation");
+    //outputString = JSON.stringify(input, null, 2)
+    //console.log(outputString); // Pretty-print the object
   } catch (e) {
     // If the input is not valid JSON, fail gracefully.
     console.error("Failed to parse or process input:", e);
@@ -23,80 +24,103 @@ function transforming(jsonBody) {
   // Required
   output["@type"] = "Dataset";
 
-  output.title = piveau.mapLiteral(input.title, output.title, params.default_language)
-  output.description = piveau.mapLiteral(input.description, output.description, params.default_language)
+
+    output.title = [
+        {
+            "@value": "Test title",
+            "@language": params.defaultLanguage
+        }
+    ];
+
+    output.description = [
+        {
+            "@value": "Test description",
+            "@language": params.defaultLanguage
+        }
+    ];
 
 
-  //dc:identifier
-  //dc:subject
-  //ows:BoundingBox
-  //ows:BoundingBox/ows:LowerCorner
-  //ows:BoundingBox/ows:UpperCorner
+
+    output.publisher = {
+            "@type": "Agent",
+            "name": "Test name",
+            "type": {
+                "@id": "http://purl.org/adms/publishertype/NationalAuthority"
+            }
+        };
+
 
   output.distribution = [];
-  for (const resource of input["csw:SearchResults"]["csw:Record"]) {
 
-    if (false) {
-    //if (resource.identifier) {
+  //for (let i = 0; i < input["csw:SearchResults"]["csw:Record"].slice(0,1); i++) {
 
-      let record_language
-      if (resource["dc:language"]) {
-        if (resource["dc:language"] === "ger") {
-          record_language = "de"
-        } else {
-          record_language = input["dc:language"];
-        }
-      } else {
-        record_language = params.default_language;
-      }
+    let resource = input["csw:SearchResults"]["csw:Record"][0];
 
-      let dist = {
-        "@type": "Distribution",
-        "accessUrl": [{
-          "@id": encodeURI("https://example.eu/set/distribution/" + resource.id)
-        }],
-        "description": [{
-          "@value": resource.description,
-          "@language": params.defaultLanguage
-        }]
-      };
-      dist.identifier = mapSingleLiteral(resource.id)
-
-      if (resource.name) {
-        dist.title = [{
-          "@value": resource.title,
-          "@language": record_language
-        }];
-      }
-
-      if (resource["modified"]) {
-        dist.issued = resource["modified"]
-      }
-      output.distribution.push(dist);
+    let i = 0
+    let id
+    if (resource["dc:identifier"]) {
+      id = resource["dc:identifier"]
+    } else {
+      id = i
     }
-  }
+
+    output.identifier = id
+    let record_language
+    if (resource["dc:language"]) {
+      if (resource["dc:language"] === "ger") {
+        record_language = "de"
+      } else {
+        record_language = input["dc:language"];
+      }
+    } else {
+      record_language = params.default_language;
+    }
 
 
+    let dist = {
+      "@type": "Distribution",
+      "identifier": id,
+      "accessUrl": [{
+        "@id": encodeURI("https://example.eu/set/distribution/" + id)
+      }],
+      "description": [{
+        "@value": "Distribution description " + i,
+        "@language": params.defaultLanguage
+      }],
+      "title": [
+        {
+          "@value": "Data",
+          "@language": "en"
+        }
+      ]
 
-  //dc:URI
-  //dc:date
-  //dc:description
-  //dc:format
-  //dc:identifier
-  //dc:subject
-  //dc:type
-  //dct:abstract
-  //dct:modified
-  //ows:BoundingBox
-  //ows:BoundingBox/ows:LowerCorner
-  //ows:BoundingBox/ows:UpperCorner
+    };
+
+    dist.format = "CSV"//resource["dc:format"][0]
+    dist.id = mapSingleLiteral(id)
+    dist.language = record_language
+    dist.issued = resource["dct:modified"]
+    dist.mediaType = "text/csv"
+    dist.license = "http://dcat-ap.de/def/licenses/dl-by-de/2.0"
+
+    if (resource.title) {
+      dist.title = [{
+        "@value": resource.title,
+        "@language": record_language
+      }];
+    }
+
+    if (resource.modified) {
+      dist.issued = resource.modified
+    }
+    output.distribution.push(dist);
+  //}
 
   console.log("Transformed output:");
-  console.log(JSON.stringify(output, null, 2));
 
-  // Use the callback to pass the result to the next step.
-  // This is the correct asynchronous pattern.
-  // callback(null, jsonBody);
+  const { "@context": context, ...outputWithoutContext } = output;
+
+  console.log(JSON.stringify(outputWithoutContext, null, 2));
 
   return output
 }
