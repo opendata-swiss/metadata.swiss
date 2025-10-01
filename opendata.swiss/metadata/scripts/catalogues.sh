ecb508a37bc136f6e1cf43e6ba0a822544e11c8e
magdasalatka <magdalena@datali.ch>
2025-10-01 14:24:09 +0200

    paths - PR feedback

--- opendata.swiss/metadata/scripts/catalogues.sh
+++ opendata.swiss/metadata/scripts/catalogues.sh
@@ -6,6 +6,6 @@ set -a
 set +a
 
 
-curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @metadata/piveau_catalogues/data-staatskanzlei-kanton-zuerich.ttl "${HUB_REPO_ENDPOINT}/catalogues/staatskanzlei-kanton-zuerich"
-curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @metadata/piveau_catalogues/data-bafu.ttl "${HUB_REPO_ENDPOINT}/catalogues/bafu"
-curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @metadata/piveau_catalogues/data-stadt-winterthur.ttl "${HUB_REPO_ENDPOINT}/catalogues/stadt-winterthur-geocat"
+curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @piveau_catalogues/data-staatskanzlei-kanton-zuerich.ttl "${HUB_REPO_ENDPOINT}/catalogues/staatskanzlei-kanton-zuerich"
+curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @piveau_catalogues/data-bafu.ttl "${HUB_REPO_ENDPOINT}/catalogues/bafu"
+curl -i -X PUT -H "X-API-Key: $API_KEY_HUB" -H "Content-Type: text/turtle" --data @piveau_catalogues/data-stadt-winterthur.ttl "${HUB_REPO_ENDPOINT}/catalogues/stadt-winterthur-geocat"
