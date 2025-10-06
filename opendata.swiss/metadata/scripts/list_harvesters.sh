ced43824be76dd9737fc98a6dcca7fb14eaa4fa1
magdasalatka <magdalena@datali.ch>
2025-10-06 15:52:55 +0200

    list of all harvesters

---
+++ opendata.swiss/metadata/scripts/list_harvesters.sh
@@ -0,0 +1,2 @@
+curl 'https://ckan.opendata.swiss/api/3/action/harvest_source_list' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
