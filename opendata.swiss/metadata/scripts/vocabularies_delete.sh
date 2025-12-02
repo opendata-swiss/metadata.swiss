874bd76bdd768cdbcfcc1e8f5c082a9d57f7dfb0
Ludovic Muller <ludovicmuller1@gmail.com>
2025-12-02 15:16:02 +0100

    ci: configure manual triggers

--- opendata.swiss/metadata/scripts/vocabularies_delete.sh
+++ opendata.swiss/metadata/scripts/vocabularies_delete.sh
@@ -7,6 +7,5 @@ set +a
 
 set -eu
 
-curl -i -X DELETE -H "X-API-Key: $PIVEAU_HUB_API_KEY" "${HUB_REPO_ENDPOINT_INT}/vocabularies/ch-licenses"
-
-curl -i -X DELETE -H "X-API-Key: $PIVEAU_HUB_API_KEY" "${HUB_REPO_ENDPOINT_INT}/vocabularies/showcase-types"
+curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/ch-licenses"
+curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/showcase-types"
