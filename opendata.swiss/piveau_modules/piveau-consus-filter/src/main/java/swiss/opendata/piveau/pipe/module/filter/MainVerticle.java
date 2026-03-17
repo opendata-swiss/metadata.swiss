a197409af731d97626514c7a644b00cd3953c38b
Giacomo Citi <giacomo.citi@zazuko.com>
2026-03-17 15:20:18 +0100

    implement example filter

--- opendata.swiss/piveau_modules/piveau-consus-filter/src/main/java/swiss/opendata/piveau/pipe/module/filter/MainVerticle.java
+++ opendata.swiss/piveau_modules/piveau-consus-filter/src/main/java/swiss/opendata/piveau/pipe/module/filter/MainVerticle.java
@@ -10,17 +10,27 @@ import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.sparql.function.library.e;
 import org.apache.jena.vocabulary.DCAT;
 import org.apache.jena.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
 
     /**
      * The main Vert.x function aka entry point to the application
@@ -34,6 +44,7 @@ public class MainVerticle extends AbstractVerticle {
         });
 
         startPromise.complete();
+        logger.info("Piveau Consus Filter started successfully.");
     }
 
     private void handlePipe(PipeContext pipeContext) {
@@ -41,7 +52,33 @@ public class MainVerticle extends AbstractVerticle {
             pipeContext.log().trace(pipeContext.getPipeManager().prettyPrint());
         }
 
-        pipeContext.pass();
+        if (Lang.NTRIPLES.getHeaderString().equals(pipeContext.getMimeType())) {
+            final Model model = Piveau.toModel(
+                pipeContext.getStringData().getBytes(),
+                Lang.NTRIPLES
+            );
+            final List<Resource> datasets = model.listResourcesWithProperty(RDF.type, DCAT.Dataset).toList();  
+            if (datasets.size() != 1) {
+                logger.info("datasets size: " + datasets.size() + ", skipping.");
+                return;
+            }
+            Resource dataset = datasets.get(0);
+
+            // this is just an example of a filter
+            List<RDFNode> landingPages = model.listObjectsOfProperty(dataset, DCAT.landingPage).toList();
+            if (landingPages.isEmpty()) {
+                logger.info("Dataset " + dataset.getURI() + " has no landing page, skipping.");
+                return;
+            };
+            
+            logger.info("passing data for dataset: " + dataset.getURI());
+            pipeContext.pass();
+        
+        } else {
+            logger.warn("Unsupported MIME type: " + pipeContext.getMimeType());
+            pipeContext.pass();
+        }
+
         
     }
 
