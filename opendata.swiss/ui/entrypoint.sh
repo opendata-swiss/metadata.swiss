853db4f96ee52757878f1575e8899b2b0e750b03
tpluscode <tpluscode@users.noreply.github.com>
2026-03-26 16:26:50 +0100

    build: apply a patch with `GITHUB_REF`

--- opendata.swiss/ui/entrypoint.sh
+++ opendata.swiss/ui/entrypoint.sh
@@ -1,3 +1,7 @@
+if [ -z "$GITHUB_REF" ]; then
+  echo "GITHUB_REF not set, defaulting to refs/heads/main"
+  GITHUB_REF="refs/heads/main"
+fi
 set -e
 
 git clone --depth 1 "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}.git" --no-checkout content
