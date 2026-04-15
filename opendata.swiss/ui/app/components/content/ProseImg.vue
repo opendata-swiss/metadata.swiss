b6dc97afa8737a72d685b2761f43e0ceabc67a22
tpluscode <tpluscode@users.noreply.github.com>
2026-04-15 21:08:40 +0200

    feat: automatic image resizing

---
+++ opendata.swiss/ui/app/components/content/ProseImg.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtImg
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    loading="lazy"
+    quality="80"
+    sizes="100vw sm:50vw md:400px"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  src: string
+  alt?: string
+  width?: string | number
+  height?: string | number
+}>()
+</script>
