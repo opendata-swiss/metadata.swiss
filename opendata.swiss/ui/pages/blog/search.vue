30100dbca4cb8c5a74f30fcc840155982e0d9b41
tpluscode <tpluscode@users.noreply.github.com>
2026-04-16 17:42:07 +0200

    fix(blog): links from blog search

--- opendata.swiss/ui/pages/blog/search.vue
+++ opendata.swiss/ui/pages/blog/search.vue
@@ -2,37 +2,30 @@
 import { homePageBreadcrumb } from '../../app/composables/breadcrumbs.js'
 import { useAsyncData, useSeoMeta } from 'nuxt/app'
 import OdsCmsSearchPage from '../../app/components/OdsCmsSearchPage.vue'
-import { useGetArticleUrl } from '../../app/composables/handbook.js'
 import type { SearchResult } from 'minisearch'
 
 const { t, locale } = useI18n()
 
-const getArticleUrl = await useGetArticleUrl()
-
-const { data: pages } = await useAsyncData('handbook-search-pages', async () => {
-  return queryCollection('handbook')
-    .select('path', 'title', 'parent', 'slug')
-    .where('path', 'LIKE', `%.${locale.value}%`)
-    .all()
-})
-
 const currentLanguage = new RegExp(`\\.${locale.value}(#.+)?$`)
-const { data, error } = await useAsyncData('blog-search', async () => {
-  const sections = await queryCollectionSearchSections('blog')
-  return (sections || [])
-    .filter(section => currentLanguage.test(section.id))
+const { data: { value: [sections, blog] }, error } = await useAsyncData('blog-search', async () => {
+  let sections = await queryCollectionSearchSections('blog')
+  sections = (sections || []).filter(section => currentLanguage.test(section.id))
+
+  const blog = await queryCollection('blog')
+    .where('path', 'IN', sections.map(section => section.id))
+    .all()
+
+  return [sections, blog]
 })
 
-const searchResultToLink = (pages: BlogCollectionItem[]) => (result: SearchResult) => {
+const searchResultToLink = (result: SearchResult) => {
   const [id, hash] = result.id.split('#')
 
   let path
-  const page = pages.find(({ path }) => result.id.startsWith(path))
-  if (page) {
-    path = getArticleUrl(page)
-  }
-  else {
-    path = id.substring(0, id.lastIndexOf('.'))
+  const slug = `${id.substring(0, id.lastIndexOf('.'))}`.split('/').pop()
+  const article = blog.find(({ path }) => result.id.startsWith(path))
+  if (article) {
+    path = '/' + ['blog', article.date.substring(0, 7), slug].join('/')
   }
   return {
     path,
@@ -64,7 +57,7 @@ useSeoMeta({
   <OdsCmsSearchPage
     :search-prompt="t('message.blog.search_prompt')"
     :breadcrumbs="breadcrumbs"
-    :data="data"
-    :search-result-to-link="searchResultToLink(pages)"
+    :data="sections"
+    :search-result-to-link="searchResultToLink"
   />
 </template>
