4cbff80f8b42510d617f4c7851569c2ad66dd153
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2026-04-17 12:41:14 +0200

    fix(handbook): full-text search (#216)
    * fix(handbook): full-text search
    
    * feat(blog): full-ext search
    
    * fix(blog): links from blog search
    
    * ci: checkout before setting up kubectl

--- opendata.swiss/ui/pages/handbook/search.vue
+++ opendata.swiss/ui/pages/handbook/search.vue
@@ -1,89 +1,50 @@
-<script setup>
-import { useRouter } from 'vue-router'
-import Fuse from 'fuse.js'
-import { debounce } from 'perfect-debounce'
-import { queryHandbook, useGetArticleUrl } from '../../app/composables/handbook.js'
-import OdsBreadcrumbs from '../../app/components/OdsBreadcrumbs.vue'
-import OdsPage from '../../app/components/OdsPage.vue'
-import OdsSearchPanel from '../../app/components/OdsSearchPanel.vue'
+<script setup lang="ts">
 import { homePageBreadcrumb } from '../../app/composables/breadcrumbs.js'
-import OdsCard from '../../app/components/OdsCard.vue'
-import OdsSearchResults from '../../app/components/OdsSearchResults.vue'
-import { useSeoMeta } from 'nuxt/app'
+import { useAsyncData, useSeoMeta } from 'nuxt/app'
+import OdsCmsSearchPage from '../../app/components/OdsCmsSearchPage.vue'
+import { queryHandbook, useGetArticleUrl } from '../../app/composables/handbook.js'
+import type { HandbookCollectionItem } from '@nuxt/content'
+import type { SearchResult } from 'minisearch'
 
 const { t, locale } = useI18n()
-const route = useRoute()
-const router = useRouter()
 
-const searchInput = ref(route.query.q)
 const getArticleUrl = await useGetArticleUrl()
 
-const onSearch = debounce((value) => {
-  searchInput.value = value
-  if (route.query.q === value?.trim()) return
-
-  router.push({
-    name: route.name,
-    query: { q: value?.trim() || undefined },
-  })
-}, 300)
-
-watch(searchInput, onSearch)
-
-const { data, error } = await useAsyncData('handbook-search', async () => {
-  const [sections, pages] = await Promise.all([
-    queryCollectionSearchSections('handbook'),
-    queryHandbook()
-      .select('path', 'title', 'parent', 'slug')
-      .all(),
-  ])
-
-  return (sections || []).map((section) => {
-    let path
-    const [id, hash] = section.id.split('#')
-
-    const page = pages.find(({ path }) => section.id.startsWith(path))
-    if (page) {
-      path = getArticleUrl(page)
-    }
-    else {
-      path = id.substring(0, id.lastIndexOf('.'))
-    }
-
-    return {
-      ...section,
-      path,
-      hash: hash ? `#${hash}` : null,
-    }
-  })
+const { data: pages } = await useAsyncData('handbook-search-pages', async () => {
+  return queryHandbook()
+    .select('path', 'title', 'parent', 'slug')
+    .where('path', 'LIKE', `%.${locale.value}%`)
+    .all()
 })
+
+const currentLanguage = new RegExp(`\\.${locale.value}(#.+)?$`)
+const { data, error } = await useAsyncData('handbook-search', async () => {
+  const sections = await queryCollectionSearchSections('handbook')
+
+  return (sections || [])
+    .filter(section => currentLanguage.test(section.id))
+})
+
+const searchResultToLink = (pages: HandbookCollectionItem[]) => (result: SearchResult) => {
+  const [id, hash] = result.id.split('#')
+
+  let path
+  const page = pages.find(({ path }) => result.id.startsWith(path))
+  if (page) {
+    path = getArticleUrl(page)
+  }
+  else {
+    path = id.substring(0, id.lastIndexOf('.'))
+  }
+  return {
+    path,
+    hash: hash ? `#${hash}` : null,
+  }
+}
 
 if (error.value) {
   console.error('Failed to fetch handbook search sections', error.value)
 }
-
-const fuse = computed(() => {
-  if (!data.value) return null
-
-  return new Fuse(data.value, {
-    keys: ['title', 'titles', 'content'],
-    ignoreDiacritics: true,
-  })
-})
-
-const articleIdPattern = new RegExp(`\\.${locale.value}(#.+)?`)
-const result = computed(() => {
-  if (!fuse.value) return []
-
-  return fuse.value
-    .search(toValue(searchInput) || '')
-    .filter(article =>
-      article.item.content
-      && article.item.level < 3
-      && articleIdPattern.test(article.item.id),
-    )
-    .slice(0, 10)
-})
 
 const breadcrumbs = [
   await homePageBreadcrumb(locale),
@@ -96,57 +57,16 @@ const breadcrumbs = [
   },
 ]
 
-watch(
-  () => route.query.q,
-  (value) => {
-    searchInput.value = value
-  },
-)
-
 useSeoMeta({
   title: `${t('message.header.navigation.search')} | ${t('message.header.navigation.handbook')} | opendata.swiss`,
 })
 </script>
 
 <template>
-  <OdsPage>
-    <template #header>
-      <OdsBreadcrumbs :breadcrumbs="breadcrumbs" />
-      <OdsSearchPanel
-        v-model:search-input="searchInput"
-        :search-prompt="t('message.handbook.search_prompt')"
-      />
-    </template>
-    <OdsSearchResults :results-count="result.length">
-      <OdsCard
-        v-for="article in result"
-        :key="article.item.id"
-        :title="article.item.title"
-        type="list"
-        clickable
-      >
-        <p>{{ article.item.content }}</p>
-
-        <template #footer-action>
-          <NuxtLinkLocale
-            :to="article.item"
-            class="btn btn--outline btn--icon-only"
-            aria-label="false"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon--base icon--ArrowRight btn__icon"
-            >
-              <path
-                xmlns="http://www.w3.org/2000/svg"
-                d="m16.444 19.204 4.066-7.044-4.066-7.044-.65.375 3.633 6.294h-15.187v.75h15.187l-3.633 6.294z"
-              />
-            </svg>
-            <span class="btn__text">{{ t('message.handbook.read_more') }}</span>
-          </NuxtLinkLocale>
-        </template>
-      </OdsCard>
-    </OdsSearchResults>
-  </OdsPage>
+  <OdsCmsSearchPage
+    :search-prompt="t('message.handbook.search_prompt')"
+    :breadcrumbs="breadcrumbs"
+    :data="data"
+    :search-result-to-link="searchResultToLink(pages)"
+  />
 </template>
