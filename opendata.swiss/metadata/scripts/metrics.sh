1191ef610e6532a1304b6a114496386d23fb3579
Ludovic Muller <ludovicmuller1@gmail.com>
2026-05-06 11:42:50 +0200

    make metrics script configurable (endpoint and key)

--- opendata.swiss/metadata/scripts/metrics.sh
+++ opendata.swiss/metadata/scripts/metrics.sh
@@ -7,4 +7,4 @@ set +a
 
 set -eu
 
-curl -i -X POST -H "X-API-Key:${PIVEAU_METRICS_CACHE_API_KEY}" http://localhost:8185/admin/refresh/
+curl -i -X POST -H "X-API-Key:${PIVEAU_METRICS_CACHE_API_KEY}" "${PIVEAU_METRICS_CACHE_ENDPOINT}/admin/refresh/"
