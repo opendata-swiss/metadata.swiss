5ec507cec150ae85d94b6752e216823a375dfa8a
Michael Rauch <git.mra@miranet.ch>
2026-07-20 15:49:45 +0200

    showcase: ShowcaseTest
    test are passing with hub-repo:3.5.2 and piveau-hub-search:5.3.5

--- opendata.swiss/metadata/piveau_systemtests/src/test/java/swiss/opendata/piveau/testbench/scenarios/odsn/ShowcaseTest.java
+++ opendata.swiss/metadata/piveau_systemtests/src/test/java/swiss/opendata/piveau/testbench/scenarios/odsn/ShowcaseTest.java
@@ -55,16 +55,15 @@ public class ShowcaseTest extends BaseSystemTest {
         org.awaitility.Awaitility.await().atMost(Duration.ofSeconds(30)).until(() -> SideEffectUtils.checkSparqlAsk(getSparqlEndpoint(), askIfShowcaseExists));
 
         // Extract the minted showcase IRI from the API
-        // TODO: ...
-        // String showcaseRdf = io.restassured.RestAssured.given().header("X-API-Key", API_KEY).accept("text/turtle").when().get("/resources/showcase?catalogId=" + catalogId + "&id=" + showcaseId).then().statusCode(200).extract().body().asString();
-        // String showcaseIRI = SideEffectUtils.extractSubjectIri(showcaseRdf, "https://example.org/Showcase");
-        // System.out.println("Minted Showcase IRI: " + showcaseIRI);
-
-        // assertNotNull(showcaseIRI);
-        // assertTrue(showcaseIRI.startsWith("https://opendata.swiss/set/data/"), String.format("%s starting with https://opendata.swiss/set/data/", showcaseIRI));
+        String showcaseRdf = io.restassured.RestAssured.given().header("X-API-Key", API_KEY).accept("text/turtle").when().get("/resources/showcase/origin?catalogId=" + catalogId + "&id=" + showcaseId).then().statusCode(200).extract().body().asString();
+        String showcaseIRI = SideEffectUtils.extractSubjectIri(showcaseRdf, "https://example.org/Showcase");
+        System.out.println("Minted Showcase IRI: " + showcaseIRI);
+        
+        assertNotNull(showcaseIRI);
+        assertTrue(showcaseIRI.startsWith("https://opendata.swiss/set/resource/showcase/"), String.format("%s starting with https://opendata.swiss/set/resource/showcase/", showcaseIRI));
 
         context.store(Goal.ODSN_SHOWCASE_CREATED, "id", showcaseId);
-        // context.store(Goal.ODSN_SHOWCASE_CREATED, "iri", showcaseIRI);
+        context.store(Goal.ODSN_SHOWCASE_CREATED, "iri", showcaseIRI);
     }
 
     @Test
@@ -83,7 +82,153 @@ public class ShowcaseTest extends BaseSystemTest {
         context.store(Goal.ODSN_SHOWCASE_INDEXED, "json", json);
     }
 
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_issued(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.issued"), notNullValue());
+    }
 
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_modified(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.modified"), notNullValue());
+    }
 
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_abstract(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.abstract.de"), equalTo("Dieses interaktive Dashboard zeigt..."));
+    }
+    
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_theme(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.categories"), hasSize(1));
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.categories[0].resource"), equalTo("http://publications.europa.eu/resource/authority/data-theme/SOCI"));
+    }
+    
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_references(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.references"), hasSize(1));
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.references"), hasItem("https://opendata.swiss/set/data/523-staatskanzlei-kanton-zuerich"));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_image(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.image"), hasSize(1));
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.image"), hasItem("https://repository-images.githubusercontent.com/788945570/img.jpg"));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_keyword(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.keywords"), hasSize(1));
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.keywords.id"), hasItem("bern"));
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.keywords.language"), hasItem("en"));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEXED)
+    public void indexShowcase_type(TestContext context) {
+        String json = context.get(Goal.ODSN_SHOWCASE_INDEXED, "json", String.class);
+        io.restassured.path.json.JsonPath jp = new io.restassured.path.json.JsonPath(json);
+        org.hamcrest.MatcherAssert.assertThat(jp.get("result.type"), equalTo("https://opendata.swiss/vocabulary/showcase-type/application"));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_SEARCH_VERIFIED)
+    @Provides(Goal.ODSN_SHOWCASE_UPDATED)
+    public void updateShowcase(TestContext context) throws IOException {
+        String catalogId = context.get(Goal.ODSN_CATALOG_SHOWCASES_CREATED, "id", String.class);
+        String showcaseId = context.get(Goal.ODSN_SHOWCASE_CREATED, "id", String.class);
+        String showcaseIRI = context.get(Goal.ODSN_SHOWCASE_CREATED, "iri", String.class);
+
+        String showcaseUpdateTurtle = ResourceUtils.loadTurtle("/showcase-mietpreisentwicklung.ttl", showcaseId, "Updated Mietpreisentwicklung in Bern");
+
+        String askIfShowcaseExists = """
+                %s
+                ASK {
+                    GRAPH ?g {
+                        <%s> a <https://example.org/Showcase> ;
+                           dct:identifier "%s" ;
+                           dct:title "Updated Mietpreisentwicklung in Bern"@de .
+                    }
+                }
+                """.formatted(PREFIXES, showcaseIRI, showcaseId);
+
+        assertFalse(SideEffectUtils.checkSparqlAsk(getSparqlEndpoint(), askIfShowcaseExists));
+
+        io.restassured.RestAssured.given().header("X-API-Key", API_KEY).contentType("text/turtle").body(showcaseUpdateTurtle).when().put("/resources/showcase?catalogId=" + catalogId + "&id=" + showcaseId).then().statusCode(is(oneOf(200, 204)));
+
+        org.awaitility.Awaitility.await().atMost(Duration.ofSeconds(30)).until(() -> SideEffectUtils.checkSparqlAsk(getSparqlEndpoint(), askIfShowcaseExists));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_UPDATED)
+    @Provides(Goal.ODSN_SHOWCASE_INDEX_UPDATED)
+    public void indexShowcaseAfterUpdate(TestContext context) {
+        String showcaseId = context.get(Goal.ODSN_SHOWCASE_CREATED, "id", String.class);
+
+        System.out.println("Checking Showcase Document after update: /resources/showcase/" + showcaseId);
+        org.awaitility.Awaitility.await().atMost(Duration.ofSeconds(30)).pollInterval(Duration.ofSeconds(2)).untilAsserted(() -> {
+            io.restassured.RestAssured.given().baseUri("http://" + getServiceHost(SEARCH_SERVICE_NAME, 8080)).port(getServicePort(SEARCH_SERVICE_NAME, 8080)).when().get("/resources/showcase/" + showcaseId).then().statusCode(200).body("result.id", equalTo(showcaseId)).body("result.title.de", equalTo("Updated Mietpreisentwicklung in Bern"));
+        });
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_INDEX_UPDATED)
+    @Provides(Goal.ODSN_SHOWCASE_DELETED)
+    public void deleteShowcase(TestContext context) {
+        String catalogId = context.get(Goal.ODSN_CATALOG_SHOWCASES_CREATED, "id", String.class);
+        String showcaseId = context.get(Goal.ODSN_SHOWCASE_CREATED, "id", String.class);
+        String showcaseIRI = context.get(Goal.ODSN_SHOWCASE_CREATED, "iri", String.class);
+
+        String askIfShowcaseExists = """
+                %s
+                ASK {
+                    GRAPH ?g {
+                        <%s> a <https://example.org/Showcase> ;
+                           dct:identifier "%s" .
+                    }
+                }
+                """.formatted(PREFIXES, showcaseIRI, showcaseId);
+
+        assertTrue(SideEffectUtils.checkSparqlAsk(getSparqlEndpoint(), askIfShowcaseExists), "showcase exists");
+
+        io.restassured.RestAssured.given().header("X-API-Key", API_KEY).when().delete("/resources/showcase/origin?catalogId=" + catalogId + "&id=" + showcaseId).then().statusCode(is(oneOf(200, 204)));
+
+        // Verify Side Effect: SPARQL
+        org.awaitility.Awaitility.await().atMost(Duration.ofSeconds(30)).until(() -> !SideEffectUtils.checkSparqlAsk(
+                getSparqlEndpoint(), askIfShowcaseExists
+        ));
+    }
+
+    @Test
+    @DependsOn(Goal.ODSN_SHOWCASE_DELETED)
+    @Provides(Goal.ODSN_SHOWCASE_INDEX_DELETED)
+    public void deleteIndexAfterShowcaseDeletion(TestContext context) {
+        String showcaseId = context.get(Goal.ODSN_SHOWCASE_CREATED, "id", String.class);
+
+        System.out.println("Checking Showcase Document after deletion: /resources/showcase/" + showcaseId);
+        org.awaitility.Awaitility.await().atMost(Duration.ofSeconds(30)).pollInterval(Duration.ofSeconds(2)).untilAsserted(() -> {
+            io.restassured.RestAssured.given().baseUri("http://" + getServiceHost(SEARCH_SERVICE_NAME, 8080)).port(getServicePort(SEARCH_SERVICE_NAME, 8080)).when().get("/resources/showcase/" + showcaseId).then().statusCode(404);
+        });
+    }
     
 }
