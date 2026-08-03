69162558f5e9df2fdb2b185a5d474069844b0048
Giacomo Citi <giacomo.citi@zazuko.com>
2026-08-03 14:58:44 +0200

    generic vocab delete

--- opendata.swiss/metadata/scripts/vocabularies_delete.sh
+++ opendata.swiss/metadata/scripts/vocabularies_delete.sh
@@ -7,6 +7,18 @@ set +a
 
 set -eu
 
-curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/ch-licenses"
-curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/showcase-types"
-curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/legal-forms"
+VOCAB_NAME="${1:-}"
+if [ -z "$VOCAB_NAME" ]; then
+  echo "No vocabulary name provided, deleting all vocabularies..."
+  for file in piveau_vocabularies/*.ttl; do
+    echo "Deleting ${HUB_REPO_ENDPOINT}/vocabularies/$(basename "$file" .ttl)"
+    curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/$(basename "$file" .ttl)"
+  done 
+else   
+    echo "Deleting ${HUB_REPO_ENDPOINT}/vocabularies/${VOCAB_NAME}"
+    curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/${VOCAB_NAME}"
+fi
+
+# curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/ch-licenses"
+# curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/showcase-types"
+# curl -i -X DELETE -H "X-API-Key: ${PIVEAU_HUB_API_KEY}" "${HUB_REPO_ENDPOINT}/vocabularies/legal-forms"
